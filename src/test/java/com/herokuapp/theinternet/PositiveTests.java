fa280c6fa6c445df76a96394226a4c9e6f23df7b
aulon <aulonibrahimaj@gmail.com>
2023-10-19 17:00:37 +0200

    added the username, password and clicked on the login button

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,6 +1,8 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -16,13 +18,20 @@ public class PositiveTests {
         System.out.println("Browser started");
 
 //       open test page
-        String url = "https://the-internet.herokuapp.com/";
+        String url = "https://the-internet.herokuapp.com/login";
         driver.get(url);
         System.out.println("Page is opened");
 
 //       enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
 //       enter password
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("SuperSecretPassword!");
+
 //       click login button
+        driver.findElement(By.cssSelector(".radius")).click();
 //       verification
 //       new url
 //       logout button is visible
